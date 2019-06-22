8530c8902f8417e2ea71eadc343e4176833a120d
Eric Clark <eclark0013@gmail.com>
2019-06-22 16:34:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,16 +15,7 @@ class String
   end
 
   def count_sentences
-    sentences=0
-    endings=[".","?","!"]
-    input_array = self.split("")
-    for i in (0..input_array.length).to_a
-      puts i
-      if endings.include?("input_array[i]") && !endings.include?("input_array[i+1]")
-        sentences+=1
-      end
-    end
-    sentences
+    self.scan(/\S+[.!?]/).count
   end
 end
 
